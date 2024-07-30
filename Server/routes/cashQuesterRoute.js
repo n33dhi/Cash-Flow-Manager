fe1f9f8fdcc985fb9b5334eaa1db2477847c7d2f
n33dhi <needhisact2022@gmail.com>
2024-07-31 02:07:09 +0530

    verify user with auth controller

--- Server/routes/cashQuesterRoute.js
+++ Server/routes/cashQuesterRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { NewRequest, History } = require("../controllers/cashQuester")
+const AuthController = require("../middlewares/authorize");
 
-router.post("/newRequest", NewRequest);
-router.get("/history", History);
+router.post("/newRequest", AuthController(['employee']), NewRequest);
+router.get("/history", AuthController(['employee']), History);
 
 module.exports = router;
