4c279a4b7bce8e487f213e37f2afe2c3cca3fb7d
n33dhi <needhisact2022@gmail.com>
2024-08-01 01:58:32 +0530

    admin flow update, delete user requests

--- Server/controllers/cashMaster.js
+++ Server/controllers/cashMaster.js
@@ -1,10 +1,56 @@
 const Request = require("../schema/requestSchema");
 const User = require("../schema/userSchema");
 
-const Dashboard = async(req, res) => {
-    res.status(200).send("Welcome!!!")
-}
+const Dashboard = async (req, res) => {
+  try {
+    const request = await Request.find({});
+    if (!request) return res.status(404).send("No record!");
+    res.status(200).json({
+        status: "Welcome!!!",
+        data: request
+    })
+  } catch (e) {
+    res.status(500).send({message: e.message});
+  }
+};
+
+const AllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    res.status(200).json({ status: "Success", data: allUsers });
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+};
+
+const DeleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User Deleted!" });
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+};
+
+const UpdateRequest = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) return res.status(500).send("Request Id required")
+    const updated = await Request.findByIdAndUpdate(id, req.body);
+    const newOne = await Request.findById(id);
+    res.status(200).json(newOne);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
+};
 
 module.exports = {
-    Dashboard,
-}
+  Dashboard,
+  AllUsers,
+  DeleteUser,
+  UpdateRequest,
+};
--- Server/routes/cashMasterRoute.js
+++ Server/routes/cashMasterRoute.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const AuthController = require("../middlewares/authorize");
-const { Dashboard } = require("../controllers/cashMaster");
+const { Dashboard, AllUsers, DeleteUser, UpdateRequest } = require("../controllers/cashMaster");
+const { Logout } = require("../controllers/onBoardController");
 
-router.get("/dashboard", AuthController(['admin']), Dashboard);
+router.use(AuthController(['admin']));
+
+router.get("/dashboard", Dashboard);
+router.get("/users", AllUsers);
+router.delete("/users/:id", DeleteUser);
+router.put("/requests", UpdateRequest);
+router.post('/logout', Logout);
+
 
 module.exports = router;
