5da69d8312f79a4bf45f75c91d1c1808b27686fc
n33dhi <needhisact2022@gmail.com>
2024-08-02 00:52:10 +0530

    db: local => cluster0

--- Server/Server.js
+++ Server/Server.js
@@ -29,7 +29,7 @@ app.use("/cashMaster", cashMasterRoute);
 
 //DB CONFIG
 mongoose
-  .connect("mongodb://localhost:27017/PettyWallet")
+  .connect(process.env.DB_STRING)
   .then(() => {
     console.log("Database Connection Establised");
     app.listen(process.env.PORT || 3002, () => {
