b248c5fe16fd38736cd620a4d2b74a1493beafff
n33dhi <needhisact2022@gmail.com>
2024-08-07 23:03:31 +0530

    app.js config

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import ErrorBoundary from './Utilities/errorBoundary';
+import AuthenticatedRoute from './Utilities/authenticateRoute';
+import MainLayout from './Utilities/mainLayout.js';
 
-import Register from "./pages/register";
-import Login from "./pages/login";
-import Dashboard from "./pages/dashboard";
-import Home from "./pages/home";
-import ForgetPassword from "./pages/forgotPassword";
-import RequestForm from "./pages/request";
+const Register = lazy(() => import('./pages/register.js'));
+const Login = lazy(() => import('./pages/login.js'));
+const ForgetPassword = lazy(() => import('./pages/forgotPassword.js'));
+const Home = lazy(() => import('./pages/home.js'));
+const RequestForm = lazy(() => import('./pages/request.js'));
+const History = lazy(() => import('./pages/history.js'));
+const Dashboard = lazy(() => import('./pages/dashboard.js'));
+const Users = lazy(() => import('./pages/users.js'));
+const Requests = lazy(() => import('./pages/requests.js'));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Routes>
-            <Route path="/register" element={<Register />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/forgetPassword" element={<ForgetPassword />}></Route>
-            <Route path="/cashQuester/home" element={<Home />}></Route>
-            <Route path="cashQuester/request" element={<RequestForm />}></Route>
-            <Route path="/cashMaster/dashboard" element={<Dashboard />}></Route>
-          </Routes>
+        <ErrorBoundary>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgetPassword" element={<ForgetPassword />} />
+              <Route path="/cashQuester/home" element={<AuthenticatedRoute><MainLayout><Home /></MainLayout></AuthenticatedRoute>} />
+              <Route path="/cashQuester/request" element={<AuthenticatedRoute><MainLayout><RequestForm /></MainLayout></AuthenticatedRoute>} />
+              <Route path="/cashQuester/history" element={<AuthenticatedRoute><MainLayout><History /></MainLayout></AuthenticatedRoute>} />
+              <Route path="/cashMaster/dashboard" element={<AuthenticatedRoute><MainLayout><Dashboard /></MainLayout></AuthenticatedRoute>} />
+              <Route path="/cashMaster/users" element={<AuthenticatedRoute><MainLayout><Users /></MainLayout></AuthenticatedRoute>} />
+              <Route path="/cashMaster/requests" element={<AuthenticatedRoute><MainLayout><Requests /></MainLayout></AuthenticatedRoute>} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          </Suspense>
+        </ErrorBoundary>
       </BrowserRouter>
     </div>
   );
