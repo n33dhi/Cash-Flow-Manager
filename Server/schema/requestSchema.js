b6308344c600b1fe06172c3a6c3d84b67c9a361b
n33dhi <needhisact2022@gmail.com>
2024-08-08 20:36:55 +0530

    add unique id for every request

--- Server/schema/requestSchema.js
+++ Server/schema/requestSchema.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+const crypto = require('crypto');
+
+const generateShortId = () => {
+    return crypto.randomBytes(2).toString('hex').toUpperCase().slice(0, 3); 
+  };
 
 const RequestSchema = new mongoose.Schema({
+    requestId: { type: String, default: generateShortId, unique: true},
     requester: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     description: {type: String, required: true},
     amount: { type: Number, required: true},
     category: { type: String, required: true, enum: [
-        'travel', 'office supplies', 'employee reimbursements'], default: 'travel'},
+        'Travel', 'Office Supplies', 'Employee Reimbursements'], default: 'travel'},
     status: { type: String, enum: ['Pending', 'Approved', 'Rejected'], default: 'Pending' },
     approvedBy: { type: String, required: true},
 }, {
