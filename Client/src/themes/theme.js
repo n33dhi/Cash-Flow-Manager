bcf805634042a8deba50c4d27997dca3920cec0c
n33dhi <needhisact2022@gmail.com>
2024-08-08 20:37:43 +0530

    added theme for table cell and chips

--- Client/src/themes/theme.js
+++ Client/src/themes/theme.js
@@ -21,6 +21,67 @@ const Theme = createTheme({
         },
       },
     },
+    MuiTableCell: {
+      styleOverrides: {
+        head: {
+          fontSize: '20px',
+          fontWeight: '700',
+        },
+        body: {
+          fontSize: '16px',
+          fontWeight: '500',
+        },
+      },
+    },
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          display: 'flex',
+          padding: '0px 5px',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '3px',
+          borderRadius: '20px',
+          fontSize: '15px',
+          fontWeight: '700',
+          '& .MuiChip-icon': {
+            fontSize: '7px',
+          },
+        },
+        colorPrimary: {
+          backgroundColor: 'rgba(255, 146, 67, 0.20)',
+          color: '#FF9243',
+          '& .MuiChip-icon': {
+            color: '#FF9243',
+          },
+        },
+        colorSuccess: {
+          backgroundColor: 'rgba(71, 255, 67, 0.20)',
+          color: '#47FF43',
+          '& .MuiChip-icon': {
+            color: '#47FF43',
+          },
+        },
+        colorError: {
+          backgroundColor: 'rgba(255, 52, 52, 0.20)',
+          color: '#FF3434',
+          '& .MuiChip-icon': {
+            color: '#FF3434',
+          },
+        },
+      },
+    },
+  },
+  // Add responsive container styles
+  spacing: 8, 
+  breakpoints: {
+    values: {
+      xs: 0,  
+      sm: 600, 
+      md: 900, 
+      lg: 1200,
+      xl: 1536 
+    },
   },
 });
 
